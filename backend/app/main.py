721dbf533591f9bc2274b82e31f41eab0b0d9cf6
Halavika Palle <halavikapalle@gmail.com>
2026-06-15 23:04:07 +0530

    cors fixed

--- backend/app/main.py
+++ backend/app/main.py
@@ -27,26 +27,20 @@ import asyncio
 app = FastAPI(
     title="Trade Finance Blockchain API"
 )
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 Base.metadata.create_all(bind=engine)
 
 
 
 
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=[
-        "https://trade-finance-blockchain-explorer-mu.vercel.app",
-        "https://trade-finance-blockchain-explorer-53lf-eoaj2sjj2.vercel.app",
-        "https://trade-finance-blockchain-explorer-sable.vercel.app",
-        "https://trade-finance-blockchain-explorer-5.vercel.app",
-        "http://localhost:5173",
-        "http://127.0.0.1:5173"
-    ],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 # ROUTES
 app.include_router(auth_router)
